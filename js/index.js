27d2c7d6ffe427b9e8deb7778153f9be114d0a1d
Juneidy <juneidy@amristar.com>
2021-07-13 11:31:49 +0800

    Updated with new API

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
-Studio.$(document)
-.ready(function(){
-	var $ = Studio.$;
-	var _ = Studio._;
-	var opt = _.urlParamObj();
+import { customWidget } from './custom.js';
 
-	var config = {
-		map: opt.id || 'AVLi4IKVzzDc1QqSK-km',
-		mapworksPath: 'https://app.dev.mapworks.io/content',
+document.addEventListener('DOMContentLoaded', function(event){
+	const { Studio } = window;
+	const { id = 'AVLi4IKVzzDc1QqSK-km' } = Studio._.urlParamObj();
+
+	const config = {
+		map: id,
+		mapworksPath: 'https://app.dev.mapworks.io',
 		access: Studio.core.Map.Access.ANONYMOUS,
 		navigationControl: false,
 		scaleControl: true,
 		toolbarControl: true,
 		zoomControl: false,
+		mapworksLoginProvider: {
+			client_id: '4td941kup5d7pq38rq7gq6utu',
+			popup_redirect_uri: 'http://localhost:8080/openId.html',
+			anonymousIdp: 'public-anonymous'
+		}
 	}
 
-	var mapFetchFail = function(response) {
-		alert('fetch failed! ' + response.statusText);
-	};
-
-	var map = window.map = new Studio.core.Map('#embed1', config);
+	const map = window.map = new Studio.core.Map('#embed1', config);
 	map.load(function(err, map) {
 		if (err) {
 			Studio.app.component.dialogue.Dialogue.alert({
@@ -29,12 +30,15 @@ Studio.$(document)
 			console.log("Map Load");
 		}
 	})
-	.once('ready', function() {
+	.once('ready', function(){
 		map.off('fetch:failed');
 		// Initialise app components
 		Studio.app.App.init(Studio, map, config);
 
 		customWidget(map);
+	})
+	.once('fetch:failed', function(response){
+		alert(`fetch failed! ${response.statusText}`);
 	});
-	map.once('fetch:failed', mapFetchFail);
+
 });
